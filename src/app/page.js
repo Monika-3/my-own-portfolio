0532f1fb131df0f96870216fd6258c6f899d8aa1
Monicka3 <monimska318@gmail.com>
2025-08-28 22:37:11 +0530

    feat: Base Setup

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,9 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="max-h-screen bg-sky-50">
-      <div className="flex justify-between">
-        <div className="flex-1">
-          <h1>MY NAME IS MONIKA SELVAM</h1>
-          <h1>MY CODE NAME IS BLACK FIRE</h1>
-          <h1>MY MISSION IS TO DO SOFTWARE DEVELOPMENT</h1>
-        </div>
-        <div className="flex-shrink-0">
-          <Image
-            src="/Anya.png"
-            height={100}
-            width={100}
-            alt="Anya Forger"
-          ></Image>
-        </div>
-      </div>
-      <div></div>
-      <div></div>
+    <div>
+      <div>INTRO</div>
+      <div>SKILLS</div>
+      <div>CONTACTS</div>
     </div>
   );
 }
